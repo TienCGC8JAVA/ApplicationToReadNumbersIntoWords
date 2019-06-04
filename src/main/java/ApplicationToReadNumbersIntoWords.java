03318a7ae2c539d0cc53adeeda218ce8cd6938fd
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-06-04 15:28:08 +0700

    thay đổi cách làm

--- src/main/java/ApplicationToReadNumbersIntoWords.java
+++ src/main/java/ApplicationToReadNumbersIntoWords.java
@@ -1,83 +1,58 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class ApplicationToReadNumbersIntoWords {
     public static void main(String[] args) {
+        CheckNumber(inputNumber());
+    }
 
-        Scanner scanner = new Scanner(System.in);
+    private static int inputNumber() {
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        System.out.print("Number to read is ");
-        int number = scanner.nextInt();
+        String sAge = null;
+        try {
+            sAge = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int nAge = Integer.parseInt(sAge);
+        return nAge;
+    }
 
+    private static void CheckNumber(int number) {
         String s;
-        if (number > 0 && number < 10) {
-            switch (number) {
-                case 1:
-                    s = "one";
-                    break;
-                case 2:
-                    s = "two";
-                    break;
-                case 3:
-                    s = "three";
-                    break;
-                case 4:
-                    s = "four";
-                    break;
-                case 5:
-                    s = "five";
-                    break;
-                case 6:
-                    s = "six";
-                    break;
-                case 7:
-                    s = "seven";
-                    break;
-                case 8:
-                    s = "eight";
-                    break;
-                case 9:
-                    s = "nine";
-                    break;
-                default:
-                    s = "out of ability";
-            }
-            System.out.println(s);
+        switch (number) {
+            case 1:
+                s = "one";
+                break;
+            case 2:
+                s = "two";
+                break;
+            case 3:
+                s = "three";
+                break;
+            case 4:
+                s = "four";
+                break;
+            case 5:
+                s = "five";
+                break;
+            case 6:
+                s = "six";
+                break;
+            case 7:
+                s = "seven";
+                break;
+            case 8:
+                s = "eight";
+                break;
+            case 9:
+                s = "nine";
+                break;
+            default:
+                s = "out of ability";
         }
-
-        if (number > 10 && number < 20) {
-            int ones = number - 10;
-            switch (ones) {
-                case 1:
-                    s = "one";
-                    break;
-                case 2:
-                    s = "two";
-                    break;
-                case 3:
-                    s = "three";
-                    break;
-                case 4:
-                    s = "four";
-                    break;
-                case 5:
-                    s = "five";
-                    break;
-                case 6:
-                    s = "six";
-                    break;
-                case 7:
-                    s = "seven";
-                    break;
-                case 8:
-                    s = "eight";
-                    break;
-                case 9:
-                    s = "nine";
-                    break;
-                default:
-                    s = "out of ability";
-            }
-            System.out.println(s + "teen");
-        }
+        System.out.println(s);
     }
 }
